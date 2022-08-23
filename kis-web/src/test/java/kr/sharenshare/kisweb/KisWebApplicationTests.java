2f64ab32863850fa16e5fbbb9eb9d6ff626a04be
duveen <duveen@duveen.me>
2022-08-23 11:16:34 +0900

    Add Base Web

---
+++ kis-web/src/test/java/kr/sharenshare/kisweb/KisWebApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.sharenshare.kisweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KisWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
